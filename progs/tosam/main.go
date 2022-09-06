ca644071678db4ac99a1606bbd2ae1e009b5194d
Simon <au649801@uni.au.dk>
2022-09-06 12:31:04 +0200

    tosam

--- progs/tosam/main.go
+++ progs/tosam/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -22,12 +23,27 @@ func main() {
 	var chrom, readName, readString string
 	var pos int
 	scanner := bufio.NewScanner(infile)
+
+	//#################################
+	ff, _ := os.Create("wrong.sam")
+	output := ""
+	//#################################
+
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		//fmt.Println(scanner.Text())
 		fmt.Sscanf(scanner.Text(), "%s %s %s %d", &chrom, &readName, &readString, &pos)
 		// Now we have the data; we just need to output it in Simple-SAM format
+		//#############################
+
+		output = output + readName + "	" + chrom + "	" + fmt.Sprint(pos+1) + "	" + strconv.Itoa(len(readString)) + "M" + "	" + readString + "\n"
+
+		//#############################
+
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	ff.WriteString(output)
+	fmt.Print(output)
 }
